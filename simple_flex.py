96884d9c5190b3f2cecc1265f73c9720367daedc
acmerfight <acmerfight@gmail.com>
2014-12-20 21:12:37 +0800

    add simple_flex

---
+++ simple_flex.py
@@ -0,0 +1,12 @@
+# coding=utf8
+
+#给定如下的正则表达式 (a|b)((c|d)*)，请完成如下练习：
+#（1）使用Thompson算法，将该正则表达式转换成非确定状态有限自动机（NFA）；
+#（2）使用子集构造算法，将该上述的非确定有限状态自动机（NFA）转换成确定状态有限自动机（DFA）；
+#（3）使用Hopcroft算法，对该DFA最小化。
+
+
+class ReToNFA(object):
+
+    def __init__(self):
+        pass
